42ba72aa8479ee0e0916c7ff9456348dfb3bcb50
binaykumar19 <binaykumar19@gmail.com>
2018-10-02 20:26:13 -0400

    Seam-CarvingSeam-Carving

--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
 #include <opencv2/opencv.hpp>
-
 #include "sc.h"
 
 using namespace cv;
 using namespace std;
 
-int main( int argc, char** argv )
-{   if(argc!=5){
+int main( int argc, char** argv ){
+    if(argc!=5){
         cout<<"Usage: ../sc input_image new_width new_height output_image"<<endl;
         return -1;
     }
-    
+
     // Load the input image
     // the image should be a 3 channel image by default but we will double check that in the seam_carving
     Mat in_image;
     in_image = imread(argv[1]/*, CV_LOAD_IMAGE_COLOR*/);
-   
-    if(!in_image.data)
-    {
+
+    if(!in_image.data){
         cout<<"Could not load input image!!!"<<endl;
         return -1;
     }
@@ -26,28 +24,26 @@ int main( int argc, char** argv )
         cout<<"Image does not have 3 channels!!! "<<in_image.depth()<<endl;
         return -1;
     }
-    
+
     // get the new dimensions from the argument list
     int new_width = atoi(argv[2]);
     int new_height = atoi(argv[3]);
-    
+
     // the output image
     Mat out_image;
-   
-    
+
+
 	if(!validation(in_image.rows, in_image.cols,new_width, new_height)){
         return -1;
     }
-    
+
     if(!seam_carving_trivial(in_image,out_image, new_width, new_height)){
         return -1;
     }
-    
+
     // write it on disk
     imwrite( argv[4], out_image);
-    
-    // also display them both
-    
+
     namedWindow( "Original image", WINDOW_AUTOSIZE );
     namedWindow( "Seam Carved Image", WINDOW_AUTOSIZE );
     imshow( "Original image", in_image );
